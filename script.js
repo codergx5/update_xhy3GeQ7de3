379a0eee0b960b2a45c7e1ac33e159f8e547488e
codergx5 <m3c1o9l2@gmail.com>
2025-08-05 21:42:41 +0330

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const CONFIG = {
     // Update this with your actual APK file name in the repository
     apkFileName: 'pingkhor-vpn.apk',
     // Update this with your actual Telegram channel link
-    telegramChannel: 'https://t.me/your_channel_username',
+    telegramChannel: 'https://t.me/pingkhor_cha/897',
     // Download delay for UX (milliseconds)
     downloadDelay: 2000
 };
